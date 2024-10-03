41c6e7787236bcc0e6800c6833427df971718ea1
ollysheriblakollysheriblak <ollysheriblakegedicvpymssed@hotmail.com>
2024-10-03 14:42:34 +0000

    Initialised

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("c62b89c806e68963ca7fc81bb5e6ff4298430d93cef71ecac2aeddb9c0536307","c62b89c806e68963ca7fc81bb5e6ff4298430d93cef71ecac2aeddb9c0536307")
+        ERC721("76c3de295f96b10b9cdd6716865a34c1d404a6b7a86e0dec07d9e9386b76a2fd","76c3de295f96b10b9cdd6716865a34c1d404a6b7a86e0dec07d9e9386b76a2fd")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
